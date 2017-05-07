fb6794b740547f4497ea3db1a39b8e642660ad9a
philipptrenz <mail@philipptrenz.de>
2017-05-08 00:35:19 +0200

    added master fader

--- app/src/main/java/io/conex/app/DevicesFragment.java
+++ app/src/main/java/io/conex/app/DevicesFragment.java
@@ -11,19 +11,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 
 import io.conex.brandnewsmarthomeapp.R;
+import io.swagger.client.ApiException;
+import io.swagger.client.ApiInvoker;
 import io.swagger.client.api.DefaultApi;
 import io.swagger.client.model.Device;
 import io.swagger.client.model.Devices;
+import io.swagger.client.model.Dimmer;
 import io.swagger.client.model.Filter;
+import io.swagger.client.model.Function;
+import io.swagger.client.model.OnOff;
+import io.swagger.client.model.Patcher;
 
 import static android.content.Context.MODE_PRIVATE;
+import static android.view.View.GONE;
+import static io.conex.app.Mode.FUNCTIONS;
 
 /**
  * Created by philipp on 06.05.17.
@@ -44,6 +58,8 @@ public class DevicesFragment extends Fragment {
 
     private Mode mode;
     private String categoryId;
+
+    private MasterFunction masterFunction;
 
     public DevicesFragment() {
     }
@@ -74,6 +90,9 @@ public class DevicesFragment extends Fragment {
         Context context = getActivity().getApplicationContext();
         SharedPreferences sharedPref = context.getSharedPreferences(getString(R.string.preferences_file_key), MODE_PRIVATE);
         apiUrl = sharedPref.getString(getString(R.string.api_url_key), "");
+
+        masterFunction = new MasterFunction(rootView, devicesList, arrayAdapter);
+
         return rootView;
     }
 
@@ -82,36 +101,46 @@ public class DevicesFragment extends Fragment {
         super.onResume();  // Always call the superclass method first
     }
 
-    public void updateListView(Mode mode, String categoryId) {
+    public void updateListView() {
+        updateListView(mode, categoryId);
+    }
+
+    public void updateListView(final Mode mode, final String categoryId) {
         this.mode = mode;
         this.categoryId = categoryId;
 
-        Filter filter = new Filter();
-        List<String> list = new ArrayList<>();
-        if (categoryId != null && !categoryId.isEmpty()) {
-            list.add(categoryId);
-        }
-        if (mode != null) {
-            switch(mode) {
-                case FUNCTIONS:
-                    filter.setFunctionIds(list);
-                    break;
-                case ROOMS:
-                    filter.setRoomIds(list);
-                    break;
-                case GROUPS:
-                    filter.setGroupIds(list);
-                    break;
-                case DEVICES:
-                    filter.setDeviceIds(list);
-                    break;
-            }
-            doApiRequest(apiUrl, filter);
-        }
-    }
+        masterFunction.showMasterFunction(mode, categoryId);
 
-    public void updateListView() {
-        updateListView(mode, categoryId);
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+
+            Filter filter = new Filter();
+            List<String> list = new ArrayList<>();
+            if (categoryId != null && !categoryId.isEmpty()) {
+                list.add(categoryId);
+            }
+            if (mode != null) {
+                switch(mode) {
+                    case FUNCTIONS:
+                        filter.setFunctionIds(list);
+                        masterFunction.showMasterFunction(mode, categoryId);
+                        break;
+                    case ROOMS:
+                        filter.setRoomIds(list);
+                        break;
+                    case GROUPS:
+                        filter.setGroupIds(list);
+                        break;
+                    case DEVICES:
+                        filter.setDeviceIds(list);
+                        break;
+                }
+                doApiRequest(apiUrl, filter);
+            }
+            }
+        });
+
     }
 
 
@@ -147,19 +176,195 @@ public class DevicesFragment extends Fragment {
                 activity.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        if (list.getDevices() != null) {
+                    if (list.getDevices() != null) {
 
-                            devicesList.clear();
-                            devicesList.addAll(list.getDevices());
-                            arrayAdapter.notifyDataSetChanged();
+                        devicesList.clear();
+                        devicesList.addAll(list.getDevices());
+                        arrayAdapter.notifyDataSetChanged();
 
-                            Log.d("api", devicesList.size()+" devices received");
-
-                        }
+                        Log.d("api", devicesList.size()+" devices received");
+                    }
                     }
                 });
             }
         }).start();
+    }
+
+    private static class MasterFunction {
+
+        private List<Device> devicesList;
+        private ArrayAdapter<Device> adapter;
+
+        private LinearLayout wrapper;
+        private Button masterOnoffOn;
+        private Button masterOnoffOff;
+        private SeekBar masterDimmer;
+        private Filter filter;
+
+         private MasterFunction(View view, final List<Device> devicesList, final ArrayAdapter<Device> adapter) {
+             this.devicesList = devicesList;
+             this.adapter = adapter;
+
+            this.wrapper = (LinearLayout) view.findViewById(R.id.function_master);
+
+            this.masterOnoffOn = (Button) view.findViewById(R.id.master_onoff_on);
+            masterOnoffOn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    OnOff update = new OnOff();
+                    update.setIsOn(true);
+                    updateApi(update);
+
+                    for (Device d : devicesList) {
+                        for (Function f : d.getFunctions()) {
+                            if (f instanceof OnOff) {
+                                ((OnOff) f).setIsOn(true);
+                            }
+                        }
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+            });
+
+            this.masterOnoffOff = (Button) view.findViewById(R.id.master_onoff_off);
+            masterOnoffOff.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    OnOff update = new OnOff();
+                    update.setIsOn(false);
+                    updateApi(update);
+
+                    for (Device d : devicesList) {
+                        for (Function f : d.getFunctions()) {
+                            if (f instanceof OnOff) {
+                                ((OnOff) f).setIsOn(false);
+                            }
+                        }
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+            });
+
+            this.masterDimmer = (SeekBar) view.findViewById(R.id.master_dimmer);
+             masterDimmer.setMax(255);
+            masterDimmer.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                }
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                }
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    int value = seekBar.getProgress();
+                    boolean isOn = value > 0 ? true : false;
+
+                    Dimmer update = new Dimmer();
+                    update.setValue(value);
+                    updateApi(update);
+
+
+                    for (Device d : devicesList) {
+                        for (Function f : d.getFunctions()) {
+                            if (f instanceof Dimmer) {
+                                ((Dimmer) f).setValue(value);
+                            }
+                            if (f instanceof OnOff) {
+                                ((OnOff) f).setIsOn(isOn);
+                            }
+                        }
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+            });
+
+            hideAll();
+        }
+
+        private void hideAll() {
+            wrapper.setVisibility(GONE);
+            masterOnoffOff.setVisibility(GONE);
+            masterOnoffOn.setVisibility(GONE);
+            masterDimmer.setVisibility(GONE);
+        }
+
+        void showMasterFunction(Mode mode, String categoryId) {
+            hideAll();
+
+            if (categoryId != null) {
+                switch(mode) {
+                    case FUNCTIONS:
+                        switch (categoryId) {
+                            case "onoff":
+                                setFilterWithFunction("onoff");
+                                wrapper.setVisibility(View.VISIBLE);
+                                masterOnoffOn.setVisibility(View.VISIBLE);
+                                masterOnoffOff.setVisibility(View.VISIBLE);
+                                break;
+
+                            case "dimmer":
+                                setFilterWithFunction("dimmer");
+                                wrapper.setVisibility(View.VISIBLE);
+                                masterDimmer.setVisibility(View.VISIBLE);
+                                break;
+                        }
+                        break;
+
+                    case ROOMS:
+                        break;
+
+                    case GROUPS:
+                        break;
+
+                    case ALL:
+                        break;
+                }
+            }
+        }
+
+        private void setFilterWithFunction(String functionId) {
+            ArrayList<String> functionIds = new ArrayList<>();
+            functionIds.add(functionId);
+            filter = new Filter();
+            filter.setFunctionIds(functionIds);
+        }
+
+        private void updateApi(final Function patchedFunction) {
+            if (patchedFunction != null && filter != null && filter.getFunctionIds() != null && filter.getFunctionIds().size() > 0) {
+
+                final Patcher patcher = new Patcher();
+                patcher.setFilter(filter);
+
+                // workaround for stupid Gson JSON parser, which absorbs function_id while doing polymorphic mapping.
+                patchedFunction.setFunctionId(patchedFunction.getClass().getSimpleName().toLowerCase());
+                patcher.setFunction(patchedFunction);
+
+                new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        if (apiUrl != null) {
+                            try {
+                                Log.d("api", ApiInvoker.serialize(patcher).toString());
+                                DefaultApi api = new DefaultApi();
+                                api.setBasePath(apiUrl);
+                                api.devicesPatch(patcher);
+                            } catch (ApiException e) {
+                                Log.e("api", e.getMessage());
+                            } catch (InterruptedException e) {
+                                Log.e("api", e.getMessage());
+                            } catch (ExecutionException e) {
+                                Log.e("api", e.getMessage());
+                            } catch (TimeoutException e) {
+                                Log.e("api", e.getMessage());
+                            }
+                        }
+                    }
+                }).start();
+            }
+
+        }
+
     }
 }
 
